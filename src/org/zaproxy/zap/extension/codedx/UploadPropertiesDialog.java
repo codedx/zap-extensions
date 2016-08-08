03fa7b818727c3721051a93debdfe159c4c38ee4
Ryan McCauley <RyanM@avi.com>
2016-08-08 18:15:09 -0400

    Add more size constraints to upload dialog

--- src/org/zaproxy/zap/extension/codedx/UploadPropertiesDialog.java
+++ src/org/zaproxy/zap/extension/codedx/UploadPropertiesDialog.java
@@ -117,6 +117,7 @@ public class UploadPropertiesDialog {
 		JTextField textField = new JTextField(base, 30);
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 1;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
 		cont.add(textField, gbc);
 
 		return textField;
@@ -126,6 +127,7 @@ public class UploadPropertiesDialog {
 		createSettingsLabel("Project: ", cont);
 				
 		JComboBox<NameValuePair> box = new JComboBox<NameValuePair>(projectArr);
+		box.setPreferredSize(new Dimension(300, 27));
 		GridBagConstraints gbc = new GridBagConstraints();
 		gbc.gridx = 1;
 		gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -158,6 +160,7 @@ public class UploadPropertiesDialog {
 		gbc.gridx = 0;
 		gbc.insets = new Insets(0, 10, 0, 0);
 		gbc.anchor = GridBagConstraints.WEST;
+		gbc.fill = GridBagConstraints.HORIZONTAL;
 		cont.add(labelField, gbc);
 	}
 
