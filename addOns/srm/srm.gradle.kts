61e19b6cb9d0c82da4067404481f23bfdfb588d8
splahoti12 <slahoti@blackduck.com>
2025-09-24 03:36:44 +0530

    Renamed the Codedx to SRM (#3)
    Renaming the codedx to SRM
    Jira :- https://blackduck.atlassian.net/browse/CDX-4387

---
+++ addOns/srm/srm.gradle.kts
@@ -0,0 +1,31 @@
+import org.zaproxy.gradle.addon.AddOnStatus
+
+description = (
+    "Includes request and response data in XML reports and provides the ability " +
+        "to upload reports directly to a Software Risk Manager server"
+)
+
+zapAddOn {
+    addOnName.set("Software Risk Manager Extension")
+    addOnStatus.set(AddOnStatus.RELEASE)
+
+    manifest {
+        author.set("Black Duck, Inc.")
+        url.set("https://www.zaproxy.org/docs/desktop/addons/srm/")
+    }
+}
+
+dependencies {
+    implementation("org.apache.httpcomponents:httpmime:4.5.13")
+    implementation("com.googlecode.json-simple:json-simple:1.1.1") {
+        // Not needed.
+        exclude(group = "junit")
+    }
+}
+
+spotless {
+    java {
+        // Don't check license nor format/style, 3rd-party add-on.
+        clearSteps()
+    }
+}
